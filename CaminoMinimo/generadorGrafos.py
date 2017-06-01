ed96a97d5af3b4ed2ada65b8f95bf4b9961b136c
RodrigoDeRosa <rodrigderosa@outlook.com>
2017-06-01 17:08:14 -0300

    Creacion archivos intervalos

--- CaminoMinimo/generadorGrafos.py
+++ CaminoMinimo/generadorGrafos.py
@@ -12,20 +12,21 @@ FILE_8 = "files/g8.txt"
 FILE_9 = "files/g9.txt"
 FILE_10 = "files/g10.txt"
 FILE_11 = "files/g11.txt"
+FILE_11 = "files/g12.txt"
 
 def main():
     creador = CreadorArchivos()
     creador.crearArchivo(10, FILE_1);
-    #creador.crearArchivo(50, FILE_2);
-    #creador.crearArchivo(100, FILE_3);
-    #creador.crearArchivo(500, FILE_4);
-    #creador.crearArchivo(1000, FILE_5);
+    creador.crearArchivo(100, FILE_2);
+    creador.crearArchivo(500, FILE_3);
+    #creador.crearArchivo(1000, FILE_4);
+    #creador.crearArchivo(3500, FILE_5);
     #creador.crearArchivo(5000, FILE_6);
-    #creador.crearArchivo(10000, FILE_7);
-    #creador.crearArchivo(50000, FILE_8);
-    #creador.crearArchivo(100000, FILE_9);
-    #creador.crearArchivo(500000, FILE_10);
-    #creador.crearArchivo(1000000, FILE_11);
-    lector = LectorArchivos()
+    #creador.crearArchivo(7500, FILE_7);
+    #creador.crearArchivo(10000, FILE_8);
+    #creador.crearArchivo(35000, FILE_9);
+    #creador.crearArchivo(50000, FILE_10);
+    #creador.crearArchivo(75000, FILE_11);
+    #creador.crearArchivo(100000, FILE_12);
 
 main();
