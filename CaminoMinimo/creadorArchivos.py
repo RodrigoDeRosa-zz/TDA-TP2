4182b21e4e0b52585adbd5764bd57236955896aa
RodrigoDeRosa <rodrigderosa@outlook.com>
2017-06-01 16:33:48 -0300

    Creador grafos

---
+++ CaminoMinimo/creadorArchivos.py
@@ -0,0 +1,41 @@
+import random
+
+TEST_FILE = "files/test.txt"
+MAX_PESO = 4
+
+class CreadorArchivos(object):
+    """
+    Crea un archivo con n grafos dirijidos completos
+    """
+
+    def __init__(self):
+        pass
+
+    def crearArchivo(self, n, filePath = TEST_FILE):
+        """
+        Crea el archivo de prueba.
+        Parametros:
+            - n {int} Cantidad de vertices
+        """
+
+        try:
+            archivo = open(filePath, "w")
+        except IOError:
+            raise ValueError, "Error al abrir el archivo!"
+
+        archivo.write(str(n) + "\n") #Cantidad de vertices
+        archivo.write(str(n*(n-1)) + "\n") #Cantidad de aristas
+        self.agregarAristas(n, archivo)
+
+        archivo.close()
+
+    def agregarAristas(self, n, archivo):
+        for i in xrange(0, n):
+            for j in xrange(0, n):
+                arista = []
+                if j == i: continue #No esta unido consigo mismo
+                arista.append(str(i)) #Vertice actual
+                arista.append(str(j)) #Vertice siguiente
+                arista.append(str(random.randint(1, MAX_PESO))) #Peso
+                linea = " ".join(arista)
+                archivo.write(linea + "\n")
---
+++ CaminoMinimo/generadorGrafos.py
@@ -0,0 +1,7 @@
+from creadorArchivos import CreadorArchivos
+
+def main():
+    creador = CreadorArchivos()
+    creador.crearArchivo(10)
+
+main();
